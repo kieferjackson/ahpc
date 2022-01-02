3862fec7c543bacd35f5e37f2310cab36abd3132
kieferjackson <87453284+kieferjackson@users.noreply.github.com>
2022-01-02 13:15:46 -0700

    Updated style and supports dynamic elements
    HTML elements can now be dynamically created using kudos.js successfully.  CSS does not yet apply to those elements, but should be added in the future.  
    Most changes are to styling in the CSS.

--- kudos.js
+++ kudos.js
@@ -1,8 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {
-    var remarks = ["Best hospice in the Valley!","Thank you, Advocate!"];
 
+document.addEventListener('DOMContentLoaded', () => {
+    remarks = ["Best hospice in the Valley!","Thank you, Advocate!"];
+
+    for (var i = 0 ; i < remarks.length ; i++) {
+        addElement(i);
+    }
+
+    function addElement (i) {
+        // create a new div element
+        const newDiv = document.createElement("div");
+
+        // Add kudos remark to new remark
+        const new_remark = document.createTextNode(`${remarks[i]}`);
+
+        // add the text node to the newly created div
+        newDiv.appendChild(new_remark);
+
+        // add the newly created element and its content into the DOM
+        const current_remark = document.getElementById("kudos");
+        document.body.insertBefore(newDiv, current_remark);
+    }
+})
+
+/*document.addEventListener('DOMContentLoaded', () => {
+    var remarks = ["Best hospice in the Valley!","Thank you, Advocate!"];
+    
     for (var i = 0 ; i < remarks.length ; i++) {
         document.getElementById("remark").innerHTML = `${remarks[i]}`;
     }
+    
 
+    
 })
+*/
