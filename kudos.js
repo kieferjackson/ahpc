df9faf8ef584daf68889681e667e55eaf210403b
kieferjackson <87453284+kieferjackson@users.noreply.github.com>
2022-01-04 16:12:36 -0700

    Made Kudos objects
    Instead of an array that only contains a kudos message, now they act as objects with an associated author, message, and source.  This is currently represented in a simplistic fashion, so should be changed in the future so that the message, author and source are treated as separate elements.  This would mean that different styles could be applied to them.

--- kudos.js
+++ kudos.js
@@ -1,8 +1,34 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-    remarks = ["Best hospice in the Valley!","Thank you, Advocate!", "Test with lots of text in the following remark", "here it goes!", "no really", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."];
+let msg = [
+    {
+        "author": "Gladys",
+        "remark": "Best hospice in the Valley!",
+        "source": "Facebook"
+    },
+    {
+        "author": "Ned",
+        "remark": "Thank you, Advocate!",
+        "source": "In-person"
+    },
+];
 
-    for (var i = 0 ; i < remarks.length ; i++) {
+document.addEventListener('DOMContentLoaded', () => {
+    remarks = ["Best hospice in the Valley!","Thank you, Advocate!", "Test with lots of text in the following remark", "no really", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."];
+    
+    let element = [
+        {
+            "id": "kudos",
+            "tag": "div"
+        },
+        {
+            "id": "affiliation",
+            "tag": "p"
+        },
+    ];
+
+    const gap = document.createElement("br");
+
+    for (var i = 0 ; i < msg.length ; i++) {
         addElement(i);
     }
 
@@ -11,15 +37,16 @@ document.addEventListener('DOMContentLoaded', () => {
         const newDiv = document.createElement("div");
 
         // Add kudos remark to new remark
-        const new_remark = document.createTextNode(`${remarks[i]}`);
-
+        //if (element.id == "kudos") {
+        const content = document.createTextNode('"' + `${msg[i].remark}` + '"' + " - " + `${msg[i].author}` + " from " + `${msg[i].source}`);
+        
         // add the text node to the newly created div
-        newDiv.appendChild(new_remark);
+        newDiv.appendChild(content);
+
         addStyle(newDiv, i);
 
         // add the newly created element and its content into the DOM
         const current_remark = document.getElementById("kudos");
-        
 
         document.body.insertBefore(newDiv, current_remark);
         
@@ -32,14 +59,17 @@ document.addEventListener('DOMContentLoaded', () => {
         rem.marginLeft = "auto";
         rem.marginRight = "auto";
         rem.width = "50%";
+        rem.position = "relative";
 
         rem.padding = "50px";
         if (iteration % 2 == 0) {
-            rem.textAlign = "left";
+            rem.textAlign = "justify";
+            rem.textAlignLast = "left";
             rem.background = "#333";
             rem.color = "#fff"; 
         } else {
-            rem.textAlign = "right";
+            rem.textAlign = "justify";
+            rem.textAlignLast = "right";
             rem.background = "#fff"; 
             rem.color = "#333";
         }
@@ -49,29 +79,8 @@ document.addEventListener('DOMContentLoaded', () => {
         rem.borderRadius = "20px";
         rem.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
 
-        /*
-        for (var i =  0 ; i < document.styleSheets.length ; i++) {
-            if (document.styleSheets[i].title == 'KUDOS PAGE') {
-                let page_view = document.styleSheets[i].cssRules;
-                //current_rem = page_view;
-                remark.setAttribute("style", page_view);
-                i = document.styleSheets.length;
-            }
-                
-        }
-        */
+        
     }
 
 })
 
-/*document.addEventListener('DOMContentLoaded', () => {
-    var remarks = ["Best hospice in the Valley!","Thank you, Advocate!"];
-    
-    for (var i = 0 ; i < remarks.length ; i++) {
-        document.getElementById("remark").innerHTML = `${remarks[i]}`;
-    }
-    
-
-    
-})
-*/
