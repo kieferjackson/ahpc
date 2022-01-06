b238d1682bf6e2d046bd7c7c34fa9326d9fbaa8c
kieferjackson <87453284+kieferjackson@users.noreply.github.com>
2022-01-05 17:38:45 -0700

    CSS applies to Dynamic Elements

--- kudos.js
+++ kudos.js
@@ -10,10 +10,14 @@ let msg = [
         "remark": "Thank you, Advocate!",
         "source": "In-person"
     },
+    {
+        "author": "Annabelle",
+        "remark": "This truly is the best hospice ever made. Amazing to work for and their treatment of patients is unparalleled in the valley. I mean, some of them (I won't name any names) literally murder people, so take that with what you will.",
+        "source": "Email"
+    },
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
-    remarks = ["Best hospice in the Valley!","Thank you, Advocate!", "Test with lots of text in the following remark", "no really", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."];
     
     let element = [
         {
@@ -37,50 +41,22 @@ document.addEventListener('DOMContentLoaded', () => {
         const newDiv = document.createElement("div");
 
         // Add kudos remark to new remark
-        //if (element.id == "kudos") {
-        const content = document.createTextNode('"' + `${msg[i].remark}` + '"' + " - " + `${msg[i].author}` + " from " + `${msg[i].source}`);
+        const content = `&quot;${msg[i].remark}&quot; - ${msg[i].author} from ${msg[i].source}`;
         
         // add the text node to the newly created div
-        newDiv.appendChild(content);
+        newDiv.innerHTML = content;
 
-        addStyle(newDiv, i);
+        if (i % 2 == 0) {
+            newDiv.classList.add("kudos_even");
+        } else {
+            newDiv.classList.add("kudos_odd");
+        }
 
         // add the newly created element and its content into the DOM
-        const current_remark = document.getElementById("kudos");
+        const current_remark = document.querySelector(".remarks");
 
-        document.body.insertBefore(newDiv, current_remark);
-        
-    }
-
-    function addStyle (element, iteration) {
-        rem = element.style;
-
-        rem.display = "block";
-        rem.marginLeft = "auto";
-        rem.marginRight = "auto";
-        rem.width = "50%";
-        rem.position = "relative";
-
-        rem.padding = "50px";
-        if (iteration % 2 == 0) {
-            rem.textAlign = "justify";
-            rem.textAlignLast = "left";
-            rem.background = "#333";
-            rem.color = "#fff"; 
-        } else {
-            rem.textAlign = "justify";
-            rem.textAlignLast = "right";
-            rem.background = "#fff"; 
-            rem.color = "#333";
-        }
-        
-        rem.fontFamily = "Noto Sans";
-        rem.fontSize = "16px"; 
-        rem.borderRadius = "20px";
-        rem.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
-
+        current_remark.append(newDiv);
         
     }
 
 })
-
