ec8ce9a5b0f71c1a1ccec1bbc64f6dba30c33cc5
kieferjackson <87453284+kieferjackson@users.noreply.github.com>
2022-01-12 16:56:06 -0700

    Misc Changes + New Pages Added

--- kudos.js
+++ kudos.js
@@ -39,8 +39,9 @@ document.addEventListener('DOMContentLoaded', () => {
         let remarkExists = msgChecker(i); // Ensures that if a kudos message has null values or empty strings, that a valid value will replace it (e.g. if there is no author, it will be listed as 'Anonymous'). However, if there is no remark, no kudos element will be generated and isEven will not be updated.
 
         if (remarkExists === true) {
-            addElement(i);
-            addAffiliation(i); // CHANGE - This uses code from addElement, should just change addElement() so that it can accommodate both functions.
+            for (var q = 0 ; q < 3 ; q++) {
+                addElement(i, q)
+            }
 
             isEven = !isEven; // Successful addition of a div element should switch the message from even to odd, odd to even, etc.
             div_num++;
@@ -48,47 +49,51 @@ document.addEventListener('DOMContentLoaded', () => {
         
     }
 
-    function addElement (i) {
+    function addElement (i, q) {
         // create a new div element
         const newDiv = document.createElement("div");
 
         // Add kudos remark to new remark
-        const content = `&quot;${msg[i].remark}&quot;`;
+        switch (q) {
+            case 0:
+                var content = `&quot;${msg[i].remark}&quot;`;
+                break;
+            case 1:
+                var content = `${msg[i].author}`;
+                break;
+            case 2:
+                var content = `${msg[i].source}`;
+                break;
+        }
         
         // add the text node to the newly created div
         newDiv.innerHTML = content;
 
-        if (isEven === true) {
-            newDiv.classList.add("kudos_even");
-        } else {
-            newDiv.classList.add("kudos_odd");
-        }
+        switch (q) {
+            case 0:
+                if (isEven === true) {
+                    newDiv.classList.add("kudos_even");
+                } else {
+                    newDiv.classList.add("kudos_odd");
+                }
 
-        // add the newly created element and its content into the DOM
-        const current_remark = document.querySelector(".remarks");
+                // add the newly created element and its content into the DOM
+                var current_remark = document.querySelector(".remarks");
+                break;
+            case 1:
+            case 2:
+                newDiv.classList.add("affiliation");
+
+                if (isEven === true) {
+                    var current_remark = document.querySelectorAll(".kudos_even")[Math.floor(div_num/2)];
+                } else {
+                    var current_remark = document.querySelectorAll(".kudos_odd")[Math.floor(div_num/2)];
+                }
+                break;
+        }
 
         current_remark.append(newDiv);
         
-    }
-
-    function addAffiliation (i) {
-        // create a new div element
-        const newDiv = document.createElement("div");
-
-        const content = `${msg[i].author} ${msg[i].source}`;
-
-        // add the text node to the newly created div
-        newDiv.innerHTML = content;
-        newDiv.classList.add("affiliation");
-
-        // add the newly created element and its content into the DOM
-        if (isEven === true) {
-            var current_remark = document.querySelectorAll(".kudos_even")[Math.floor(div_num/2)];
-        } else {
-            var current_remark = document.querySelectorAll(".kudos_odd")[Math.floor(div_num/2)];
-        }
-        
-        current_remark.append(newDiv);
     }
 
     function msgChecker (i) {
