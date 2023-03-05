4eb9e9e2a8f3df77a090733d34a272dd21d4f6f2
kieferjackson <kieferleejackson@gmail.com>
2023-03-05 13:48:02 -0700

    Implemented potential fix for nav anchors + styling changes

---
+++ assets/js/offset_nav_anchor.js
@@ -0,0 +1,69 @@
+
+function find_css_rule(stylesheet, selected_css_rule)
+{
+    const stylesheet_rules = Array.from(stylesheet);
+    
+    // Loop over stylesheet rules to find selected css rule and return its rule object and index
+    for (let i = 0 ; i < stylesheet_rules.length ; i++)
+    {
+        const rule = stylesheet_rules[i];
+
+        if (rule.selectorText === selected_css_rule) {
+            return { rule, index: i };
+        }
+    }
+    
+    // The selected css rule could not be found, log error message
+    console.error(new Error(`The CSS rule ${selected_css_rule} could not be found.`));
+}
+
+function determineTopNavbarHeight()
+{
+    // Get Top Navbar to find its current height
+    const [ top_navbar ] = document.getElementsByClassName('top_navbar');
+    
+    const top_navbarHeight = top_navbar.clientHeight;
+    const top_navbarBorderTop = parseInt(
+        // Obtain the `border-top` property value for the Top Navbar element
+        window.getComputedStyle(top_navbar, null).getPropertyValue('border-top')
+    );
+    
+    return top_navbarHeight + top_navbarBorderTop;
+}
+
+const get_ahpc_stylesheet = () => {
+    const AHPC_STYLE = 'ahpc_style.css';
+
+    try
+    {
+        const { styleSheets } = document;
+
+        const [ ahpc_style ] = Array.from(styleSheets).filter(styleSheet => styleSheet.href.includes(AHPC_STYLE));
+        
+        if (!ahpc_style)
+        {
+            console.error(
+                new Error(`The stylesheet ${AHPC_STYLE} could not be found. 
+                \nCheck that the file is available and correctly named.`)
+            );
+
+            return;
+        }
+
+        return ahpc_style;
+    }
+    catch (error)
+    {
+        console.error(error);
+    }
+}
+
+// Select `nav_anchor` css rule
+const ahpc_style = get_ahpc_stylesheet();
+const nav_anchor_rule = find_css_rule(ahpc_style.cssRules, '.nav_anchor');
+const { rule, index } = nav_anchor_rule;
+
+// Set the offset for the element's `top` styling property by the determined navbar height
+rule.style.top = `-${Math.floor(determineTopNavbarHeight() * 1.25)}px`;
+// Update the stylesheet with the updated Nav Anchor rule at its associated index
+ahpc_style.cssRules[index] = rule;
