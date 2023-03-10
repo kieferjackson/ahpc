d499804ac37674f6c8bdb93e16d3341a04114498
kieferjackson <kieferleejackson@gmail.com>
2023-03-09 17:53:25 -0700

    Implemented potential fix for mobile form anchors

--- assets/js/offset_nav_anchor.js
+++ assets/js/offset_nav_anchor.js
@@ -13,6 +13,30 @@ function find_css_rule(stylesheet, selected_css_rule)
     console.error(new Error(`The CSS rule ${selected_css_rule} could not be found.`));
 }
 
+function find_media_rule(media_stylesheet, selected_css_rule)
+{
+    const media_rules = [];
+
+    // Find the Media Rules in the given stylesheet
+    for (let i = 0 ; i < media_stylesheet.length ; i++)
+    {
+        const media_rule = media_stylesheet[i];
+
+        if (media_rule instanceof CSSMediaRule) {
+            media_rules.push({ media_rule, media_index: i });
+        }
+    }
+    
+    // Find the selected css rules out of the media rule(s)
+    const [ css_rule ] = media_rules.map(({ media_rule, media_index }) => {
+        const { rule, index } = find_css_rule(media_rule.cssRules, selected_css_rule)
+
+        return { rule, index, media_index };
+    });
+    
+    return css_rule;
+}
+
 function determineTopNavbarHeight()
 {
     // Get Top Navbar to find its current height
@@ -22,23 +46,17 @@ function determineTopNavbarHeight()
     return top_navbarHeight;
 }
 
-const get_ahpc_stylesheet = () => {
-    const AHPC_STYLE = 'ahpc_style.css';
-
+const getStylesheet = (stylesheet_filename) => {
     try
     {
         const { styleSheets } = document;
 
-        const [ ahpc_style ] = Array.from(styleSheets).filter(({ href }) => href.includes(AHPC_STYLE));
+        const [ ahpc_style ] = Array.from(styleSheets).filter(({ href }) => href.includes(stylesheet_filename));
         
         if (!ahpc_style)
         {
-            console.error(
-                new Error(`The stylesheet ${AHPC_STYLE} could not be found. 
-                \nCheck that the file is available and correctly named.`)
-            );
-
-            return;
+            throw new Error(`The stylesheet ${stylesheet_filename} could not be found. 
+            \nCheck that the file is available and correctly named.`);
         }
 
         return ahpc_style;
@@ -50,12 +68,23 @@ const get_ahpc_stylesheet = () => {
 }
 
 // Select `nav_anchor` css rule
-const ahpc_style = get_ahpc_stylesheet();
+const ahpc_style = getStylesheet('ahpc_style.css');
 const ahpc_rules = ahpc_style.cssRules || ahpc_style.rules;
 const nav_anchor_rule = find_css_rule(ahpc_rules, '.nav_anchor');
-const { rule, index } = nav_anchor_rule;
+
+// Select `assessment_anchor` and `form_anchor` (nav_anchor override) css rules from Media rules
+const eligibility_style = getStylesheet('eligibility.css');
+const eligibility_rules = eligibility_style.cssRules || eligibility_style.rules;
+const override_rules = find_media_rule(eligibility_rules, '.assessment_anchor, .form_anchor');
+
+const top_navbar_height = determineTopNavbarHeight();
 
 // Set the offset for the element's `top` styling property by the determined navbar height
-rule.style.top = `-${Math.floor(determineTopNavbarHeight() * 1.25)}px`;
+nav_anchor_rule.rule.style.top = `-${Math.floor(top_navbar_height * 1.25)}px`;
 // Update the stylesheet with the updated Nav Anchor rule at its associated index
-ahpc_style.cssRules[index] = rule;
+ahpc_style.cssRules[nav_anchor_rule.index] = nav_anchor_rule.rule;
+
+// Set rules for mobile media rules
+override_rules.rule.style.top = `-${Math.floor(top_navbar_height * 2.5)}px`;
+// Update the stylesheet with the updated Form Anchor and Assessment Anchor at their associated index
+eligibility_style.cssRules[override_rules.media_index].cssRules[override_rules.index] = override_rules.rule;
