d2c375f3c60f888c16b14a05f192440257f0ffcf
kieferjackson <kieferleejackson@gmail.com>
2023-03-10 17:25:10 -0700

    Removed unneeded code from offset script

--- assets/js/offset_nav_anchor.js
+++ assets/js/offset_nav_anchor.js
@@ -13,30 +13,6 @@ function find_css_rule(stylesheet, selected_css_rule)
     console.error(new Error(`The CSS rule ${selected_css_rule} could not be found.`));
 }
 
-function find_media_rule(media_stylesheet, selected_css_rule)
-{
-    const media_rules = [];
-
-    // Find the Media Rules in the given stylesheet
-    for (let i = 0 ; i < media_stylesheet.length ; i++)
-    {
-        const media_rule = media_stylesheet[i];
-
-        if (media_rule instanceof CSSMediaRule) {
-            media_rules.push({ media_rule, media_index: i });
-        }
-    }
-    
-    // Find the selected css rules out of the media rule(s)
-    const [ css_rule ] = media_rules.map(({ media_rule, media_index }) => {
-        const { rule, index } = find_css_rule(media_rule.cssRules, selected_css_rule)
-
-        return { rule, index, media_index };
-    });
-    
-    return css_rule;
-}
-
 function determineTopNavbarHeight()
 {
     // Get Top Navbar to find its current height
@@ -72,24 +48,7 @@ const ahpc_style = getStylesheet('ahpc_style.css');
 const ahpc_rules = ahpc_style.cssRules || ahpc_style.rules;
 const nav_anchor_rule = find_css_rule(ahpc_rules, '.nav_anchor');
 
-// Get top navbar height, including height from padding and horizontal scroll bar
-const top_navbar_height = determineTopNavbarHeight();
-
 // Set the offset for the element's `top` styling property by the determined navbar height
-nav_anchor_rule.rule.style.top = `-${Math.floor(top_navbar_height * 1.25)}px`;
+nav_anchor_rule.rule.style.top = `-${Math.floor(determineTopNavbarHeight() * 1.25)}px`;
 // Update the stylesheet with the updated Nav Anchor rule at its associated index
 ahpc_style.cssRules[nav_anchor_rule.index] = nav_anchor_rule.rule;
-
-// Set Form & Assessment Anchor Rules if on /eligibility
-const eligibility_style = getStylesheet('eligibility.css');
-if (eligibility_style)
-{
-    // Select `assessment_anchor` and `form_anchor` (nav_anchor override) css rules from Media rules
-    const eligibility_rules = eligibility_style.cssRules || eligibility_style.rules;
-    const override_rules = find_media_rule(eligibility_rules, '.assessment_anchor, .form_anchor');
-
-    // Set rules for mobile media rules
-    override_rules.rule.style.top = `-${Math.floor(top_navbar_height * 2.5)}px`;
-    // Update the stylesheet with the updated Form Anchor and Assessment Anchor at their associated index
-    eligibility_style.cssRules[override_rules.media_index].cssRules[override_rules.index] = override_rules.rule;
-}
